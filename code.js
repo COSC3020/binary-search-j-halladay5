6dfc1beb55f68e1af604e45e8df7a75f0818b28f
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-02-08 10:27:46 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ function binarySearch(list, element) {
     var hi = list.length - 1;
     var lo = 0;
     var mid;
+    if(list.length == 0) return -1;
     while(hi > lo){
         mid = Math.floor((hi + lo)/ 2);
         if(list[mid] == element){ 

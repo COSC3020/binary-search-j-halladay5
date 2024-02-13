fa8c6491c19cdfc7ae1cd1fb7f90739bd719a11b
j-halladay5 <156850724+j-halladay5@users.noreply.github.com>
2024-02-12 22:28:35 -0700

    Update code.js with returning multiple values

--- code.js
+++ code.js
@@ -2,10 +2,25 @@ function binarySearch(list, element) {
     var hi = list.length - 1;
     var lo = 0;
     var mid;
+    var indices = [];
+    
     while(hi >= lo){
         mid = Math.floor((hi + lo)/ 2);
         if(list[mid] == element){ 
-            return mid;
+            let left = mid-1;
+            indices.push(mid);
+            while((left >= 0) && (list[left] == element)){
+                indices.push(left);
+                left--;
+            }
+
+            let right = mid+1;
+            while((right< list.length) && (list[right] == element)){
+                indices.push(right);
+                right++;
+            }
+            
+            return indices;
         }else if(list[mid] > element){
             hi = mid-1;
         }else{
@@ -14,3 +29,8 @@ function binarySearch(list, element) {
     }
     return -1;
 }
+
+//https://www.geeksforgeeks.org/javascript-return-multiple-values-from-function/
+// Although the search was working correctly, it failed when the list only consisted of multiple values of the element
+// I researched the return function and found it could only return one item, and this website gave me the idea of an array 
+// to return multiple values and told me I couldn't return multiple numbers at a time.
